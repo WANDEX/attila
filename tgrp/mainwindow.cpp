608cd270526378f1b663d52fb52167e45c4e5b91
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-06-01 18:28:44 +0300

    display tasks between dates: from-to on dateChanged signal

--- tgrp/mainwindow.cpp
+++ tgrp/mainwindow.cpp
@@ -1,16 +1,29 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "tgrp.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    startup();
+
+    connect(ui->dateFr, &QDateEdit::dateChanged, this, &MainWindow::dateSpanChanged);
+    connect(ui->dateTo, &QDateEdit::dateChanged, this, &MainWindow::dateSpanChanged);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::startup()
+{
+    setLastWeekSpan();
+    std::pair<std::string, std::string> datestr_fr_to = getDateFrTo();
+    std::vector<Task> tasks = file_tasks(find_last_week_file());
+    setTxt(QString::fromStdString(tasks_to_mulstr(tasks)));
 }
 
 void MainWindow::setLastWeekSpan()
@@ -33,4 +46,18 @@ std::pair<std::string, std::string> MainWindow::getDateFrTo()
 void MainWindow::setTxt(const QString &txt)
 {
     ui->plainTextEdit->setPlainText(txt);
+}
+
+void MainWindow::dateSpanChanged()
+{
+    // TODO: verify that fr/to is really changed -> return otherwise
+
+    date_fr = ui->dateFr->date();
+    date_to = ui->dateTo->date();
+
+    std::string fr = date_fr.toString("yyyy-MM-dd").toStdString();
+    std::string to = date_to.toString("yyyy-MM-dd").toStdString();
+    std::string content = concat_span(fr, to);
+
+    setTxt(QString::fromStdString(content));
 }
--- tgrp/mainwindow.h
+++ tgrp/mainwindow.h
@@ -2,8 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
 
 #include <QDate>
+
+#include "tgrp.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,15 +20,16 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-void setLastWeekSpan();
-
-std::pair<std::string, std::string> getDateFrTo();
-
-void setTxt(const QString &txt);
-    QString txt() const;
+private slots:
+    void dateSpanChanged();
 
 private:
     Ui::MainWindow *ui;
+
+    void startup();
+    void setLastWeekSpan();
+    void setTxt(const QString &txt);
+    std::pair<std::string, std::string> getDateFrTo();
 
     QDate date_fr;
     QDate date_to;
