f78e456b1c048fdb995b3e97171055717c4821b9
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-06-23 19:59:25 +0300

    [structs.hpp] task_t: id, operator< for using in sets, set sub tasks

--- tgrp/structs.hpp
+++ tgrp/structs.hpp
@@ -1,12 +1,18 @@
 #ifndef STRUCTS_HPP
 #define STRUCTS_HPP
 
+#include <atomic>  // atomic, fetch_add
 #include <cstddef> // size_t
+#include <set>
 #include <string>
 #include <vector>
 
 namespace ss
 {
+    inline std::uint32_t getID() {
+        static std::atomic<std::uint32_t> uid { 0 };
+        return uid.fetch_add(1, std::memory_order_relaxed);
+    }
 
     struct hm_t {
         int h;
@@ -21,9 +27,16 @@ namespace ss
         ss::hm_t    hm_t;
         std::vector<std::string> words;
         std::vector<std::string> tproj;
+        std::uint32_t id { ss::getID() };
+        std::set<ss::task_t> subt_t {};
     };
 
+    inline bool operator<(const ss::task_t &lhs, const ss::task_t &rhs) {
+        return lhs.id < rhs.id;
+    }
+
     using vtasks_t = std::vector<ss::task_t>;
+    using stasks_t = std::set<ss::task_t>;
 
     struct stats_t {
         const std::size_t avg;
