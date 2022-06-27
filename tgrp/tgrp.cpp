20d94cef0dbb9f53f94c1f435161485e7f22175a
WANDEX <15724752+WANDEX@users.noreply.github.com>
2022-06-27 14:07:22 +0300

    [tgrp.cpp] removed floor(), initialize tm variables with {}

--- tgrp/tgrp.cpp
+++ tgrp/tgrp.cpp
@@ -1,4 +1,3 @@
-#include <cmath>    // floor
 #include <ctime>    // difftime, mktime
 
 #include <fstream>
@@ -39,32 +38,32 @@ std::vector<int> split_vi(const std::string &s, char delimiter)
 
 const ss::hm_t calculate_time_spent(const std::string &fr, const std::string &to)
 {
-    std::vector<int> fr_res = split_vi(fr, ':');
-    std::vector<int> to_res = split_vi(to, ':');
+    std::vector<int> fr_res { split_vi(fr, ':') };
+    std::vector<int> to_res { split_vi(to, ':') };
 
 #if 0
     fmt::print("fr:{},{}\n", fr_res[0], fr_res[1]);
     fmt::print("to:{},{}\n", to_res[0], to_res[1]);
 #endif
 
-    std::tm t1 = {};
+    std::tm t1 {};
     t1.tm_hour = fr_res[0];
     t1.tm_min  = fr_res[1];
     std::time_t beg = std::mktime(&t1);
 
-    std::tm t2 = {};
+    std::tm t2 {};
     t2.tm_hour = to_res[0];
     t2.tm_min  = to_res[1];
     std::time_t end = std::mktime(&t2);
 
-    int sec_diff = std::floor(std::difftime(end, beg));
+    int sec_diff = std::difftime(end, beg);
     if (sec_diff == 0) return {0, 0, 0, "00:00"};
     // fix: 23:53 -> 00:07 expected time spent: (00:14)
     // recalculate if the task was ended the next day
     if (sec_diff < 1) {
         t2.tm_mday = t2.tm_mday + 1;
         end = std::mktime(&t2);
-        sec_diff = std::floor(std::difftime(end, beg));
+        sec_diff = std::difftime(end, beg);
     }
     int h = sec_diff / 3600;
     int m = sec_diff % 3600 / 60;
@@ -154,7 +153,7 @@ ss::vtasks_t parse_tasks_parallel(const std::string &s)
     while (std::getline(content, line))
         lines.push_back(line);
     // lines per thread (-1 thread) & remainder
-    size_t lpt = std::floor(nl / num_threads);
+    size_t lpt = nl / num_threads;
     double lpt_remainder = nl % num_threads;
     // lambda function for feeding the tasks analyzer
     // with equally distributed chunks-lines of one large text
@@ -226,7 +225,7 @@ std::vector<std::string> find_week_files(const std::string &pmatch = "week-")
 */
 std::string week_file_name(const std::string &date_str)
 {
-    std::tm tm1 = {}, tm2 = {};
+    std::tm tm1 {}, tm2 {};
     const char* wfmt = "week-%V-%Y.txt";
     const std::time_t now = std::time(nullptr);
     tm1 = *std::localtime(&now);
